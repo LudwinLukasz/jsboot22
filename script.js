2fc28bfdbddf0f0bb6455ec9b73688de331bb75a
Lukasz Ludwin <gorli@interia.pl>
2017-06-19 23:25:59 +0200

    second commit

--- script.js
+++ script.js
@@ -18,15 +18,15 @@ var MovieDescription = React.createClass({
 
 var Movie = React.createClass({
 	propTypes: {
-    movie: React.PropTypes.object.isRequired,
-  },
+		movie: React.PropTypes.object.isRequired,
+	},
 	render: function() {
 		return React.createElement('li', {key: this.props.movie.id},
 			React.createElement(MovieTitle, {title: this.props.movie.title}),
 			React.createElement(MovieDescription, {description: this.props.movie.description}),
 			React.createElement('img', {src: this.props.movie.poster})
 		)
- 	}
+	}
 });
 
 var MoviesList = React.createClass({
@@ -41,26 +41,26 @@ var MoviesList = React.createClass({
 	}
 });
 
-var film1 = {
+var films = [
+	{
 		id: 1,
 		title:'Harry Potter',
 		description:'Film o czarodzieju',
 		poster:'henryk.jfif'
-};
-var film2 = {
+	},
+	{
 		id: 2,
 		title:'Król lew',
 		description:'Film o lwie',
 		poster:'krol.jpg'
-};
-var film3 = {
+	},
+	{
 		id: 3,
 		title:'W pustyni i w puszczy',
 		description:'Film o lwie i innych takich',
 		poster:'pustynia.jfif'
-};
-
-var films = [film1, film2, film3];
+	}
+]
 
 var filmlist = React.createElement(MoviesList, {items: films});
 ReactDOM.render(filmlist, document.getElementById('app'));
